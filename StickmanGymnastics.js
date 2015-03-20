adff390e4c8c2e1bafa13019a96f60b0d2262a18
Jonathan Wilson <jdw7@rice.edu>
2015-03-20 12:40:59 -0500

    Made basic version of freeplay.

--- StickmanGymnastics.js
+++ StickmanGymnastics.js
@@ -3,6 +3,10 @@
 // imported.src = '../StickmanGymnasticsHeader.js';
 // document.getElementsByTagName('script')[0].parentNode.appendChild(script);
 
+///////////
+//objects//
+///////////
+var man = new Man();
 
 ///////////////////
 //event listeners//
@@ -12,14 +16,14 @@ window.addEventListener("keydown", function(event){
 });
 
 window.addEventListener("keyup", function(event){
+    //if ((event.keyCode == 37) || (event.keyCode == 39)){
+    //    man.stop_running();
+    //}
 	delete keysDown[event.keyCode];
 });
 
 
-///////////
-//objects//
-///////////
-var man = new Man();
+
 
 
 
@@ -56,35 +60,45 @@ var update = function(){
 			man.set_state(TUCKED);
 		}
 		if (key == 37){
-			man.set_left();
+            if (Object.keys(keysDown).length == 1) {
+                man.set_state(RUNNING);
+            }
+            man.set_left();
 		}
 		if (key == 39){
-			man.set_right();
+            if (Object.keys(keysDown).length == 1) {
+                man.set_state(RUNNING);
+            }
+            man.set_right();
 		}
+        if (key == 32){
+            man.set_jump();
+        }
 
 	}
 
 	//IN_GAME:
 
 
-	if (!man.JUMP){
-		man.ONGROUND = man.is_on_ground();
-	}
+    //if (!man.JUMP){
+	man.ONGROUND = man.is_on_ground();
+	//}
+	//var wasOnGround = man.ONGROUND;
 
 	man.conform_rigid_man(); //get new body angles
 	man.update_limb_angles(); //update body's configuration, but still upright
 
+	//if (wasOnGround){
+	//	man.ONGROUND = wasOnGround;
+	//}
 	//stickman is in the air
-	if (!man.ONGROUND){	
-		//man.update_diff_by_accel(g);
+	if (!man.ONGROUND){
+        man.JUMP = false;
+		man.update_diff_by_accel(g);
 		// CASE 1 : He is still in the air
 		
 		man.rotate(man.get_location()); //rotate man
 
-		if (man.is_on_ground()){
-			man.ONGROUND = true;
-
-		}
 		// CASE 2 : He has just landed
 	}
 
@@ -100,23 +114,34 @@ var update = function(){
 
 
 		// CASE 1 : JUMP
-
+        if (man.JUMP){
+            man.jump();
+            man.update_diff_by_accel(g);
+        }
 		// CASE 2 : RUN
 
 		// CASE Else : Rotate stickman about ground fulcrum
 		//determine fulcrum
-		man.update_diff_by_cm(); //update cm based on body position
 		//put him back on the ground on the fulcrum
 		//update fulcrum (it may have changed after conforming rigid man) ??? When to do conform_rigid_man()?
 
 	}
-
-
+    man.update_diff_by_cm(); //update cm based on body position
+    if (man.fulcrumIndex == 10){
+        var head_pos = man.get_head_pos();
+        if (man.get_center_of_mass()[0] < head_pos[0]) {
+            man.rotate_rigid_man([head_pos[0] + man.diff[0], head_pos[1] + man.diff[1]], -.03);
+        }
+        else{
+            man.rotate_rigid_man([head_pos[0] + man.diff[0], head_pos[1] + man.diff[1]], .03);
+        }
+    }
 }
 
 var render = function(){
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	man.draw();
+    draw_ground();
 }
 
 
--- StickmanGymnasticsHeader.js
+++ StickmanGymnasticsHeader.js
@@ -11,8 +11,8 @@ var animate = window.requestAnimationFrame ||
 
 //canvas
 var canvas = document.createElement('canvas');
-var width = 600;
-var height = 800;
+var width = 1000;
+var height = 400;
 canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
@@ -21,14 +21,14 @@ var context = canvas.getContext('2d');
 var keysDown = {};
 
 
-var xo = 300;
-var yo = 450;
+var xo = width/2;
+var yo = height/2;
 var rad = Math.PI/180;
-var g = [0,5];
+var g = [0,3];
 var headWeight = 80;
 var dt = .2;
 
-var ground = height - 20;
+var ground = height - 10;
 
 var STANDING = '';
 var LONG = 'w';
@@ -46,7 +46,7 @@ var vector_to_angle = function(vector){
 	if (get_dist(vector,[0,0]) < Math.pow(10,-5)){
 		return 0;
 	}
-	return Math.atan2(vector[1], vector[0]);
+	return Math.atan2(vector[0], vector[1]);
 }
 var angle_to_vector = function(angle){
 	//alert(angle);
@@ -60,8 +60,22 @@ var get_dist = function(vector1, vector2){
 	catch(err){
 		document.write("Error: get_dist requires two 2d arrays");
 	}
-
 }
+var draw_ground = function(){
+    context.beginPath();
+	context.moveTo(0, ground);
+	context.lineTo(width, ground);
+	context.stroke();
+    context.beginPath();
+	context.moveTo(0, ground);
+	context.lineTo(0, 0);
+	context.stroke();
+    context.beginPath();
+	context.moveTo(width, ground);
+	context.lineTo(width, 0);
+	context.stroke();
+}
+
 
 /////////////////////
 //class definitions//
@@ -75,21 +89,59 @@ function Limb(angle,length){
 
 }
 Limb.prototype.draw = function(offset){
-	//var ctx=canvas.getContext("2d");
-	context.beginPath();
-	//document.write("pos1 = " + this.pos1);
-	//document.write("pos2 = " + this.pos2);
-	// var vec = angle_to_vector(baseAngle + this.angle);	
-	// var pos2 = [basePoint[0] + this.len * vec[0], basePoint[1] + this.len * vec[1]];
+    if(Math.floor((this.pos1[0] + offset[0])/width) == Math.floor((this.pos2[0] + offset[0])/width)) {
+        context.beginPath();
+        context.moveTo((this.pos1[0] + offset[0]) % width, this.pos1[1] + offset[1]);
+        context.lineTo((this.pos2[0] + offset[0]) % width, this.pos2[1] + offset[1]);
+        context.stroke();
+    }
+    else{
+        if (offset[0] + this.pos1[0] < 0){
+            offset[0] = width - Math.abs(offset[0]);
+        }
+        if (offset[0] + this.pos2[0] < 0){
+            offset[0] = width - Math.abs(offset[0]);
+        }
+        var p1 = [(this.pos1[0] + offset[0]), this.pos1[1] + offset[1]];
+        var p2 = [(this.pos2[0] + offset[0]), this.pos2[1] + offset[1]];
 
-	// context.moveTo(basePoint[0] + offset[0], basePoint[1] + offset[1]);
-	// context.lineTo(pos2[0] + offset[0], pos2[1] + offset[1]);
-	context.moveTo(this.pos1[0] + offset[0], this.pos1[1] + offset[1]);
-	context.lineTo(this.pos2[0] + offset[0], this.pos2[1] + offset[1]);
+        p1[0] = p1[0] - width * Math.floor(p1[0]/width);
+        p2[0] = p2[0] - width * Math.floor(p2[0]/width);
 
 
 
-	context.stroke();
+        if (p1[0] > p2[0]) {
+            //if (Math.abs(p1[0] - width) <= Math.max(this.limbLengths)) {
+                context.beginPath();
+                context.moveTo(p1[0], this.pos1[1] + offset[1]);
+                context.lineTo(width, this.pos2[1] + offset[1]);
+                context.stroke();
+            //}
+            //if (Math.abs(0 - p2[0]) <= Math.max(this.limbLengths)) {
+                context.beginPath();
+                context.moveTo(0, this.pos1[1] + offset[1]);
+                context.lineTo(p2[0], this.pos2[1] + offset[1]);
+                context.stroke();
+            //}
+        }
+        else {
+            //if (Math.abs(0 - p1[0]) <= Math.max(this.limbLengths)) {
+                context.beginPath();
+                context.moveTo(p1[0], this.pos1[1] + offset[1]);
+                context.lineTo(0, this.pos2[1] + offset[1]);
+                context.stroke();
+            //}
+            //if (Math.abs(width - p2[0]) <= Math.max(this.limbLengths)) {
+                context.beginPath();
+                context.moveTo(width, this.pos1[1] + offset[1]);
+                context.lineTo(p2[0], this.pos2[1] + offset[1]);
+                context.stroke();
+            //}
+        }
+
+    }
+
+
 }
 Limb.prototype.get_pos1 = function(){
 	return this.pos1;
@@ -123,11 +175,12 @@ function Man(){
 	this.JUMP, this.ONGROUND = false;
 	this.RIGHT = true;
 	this.STATE = STANDING;
-	this.net_body_change = 0;
 	this.neckBase = [xo,yo];
 	this.limbList = [];
-	this.limbAngles = [-30*rad,-45*rad,10*rad,10*rad,35*rad,3*rad,10*rad,-25*rad,-5*rad];
+	this.limbAngles = [0*rad,-45*rad,10*rad,10*rad,35*rad,3*rad,10*rad,-25*rad,-5*rad];
 	this.limbLengths = [36,22,22,18,18,24,24,30,30];
+    this.runTime = 0;
+    this.stridePeriod = 30;
 	//alert("rad = " + rad);
 	//alert("this.limbAngles[0] = " + this.limbAngles[0]);
 
@@ -135,20 +188,30 @@ function Man(){
 	this.vel = [0,0];
 	this.diff = [0,0]; //this will be updated as stickman moves
 	this.angle = 0;
-	this.rotationalMomentum = 2 * Math.pow(10,6);
+	this.rotationalMomentum = 0 * Math.pow(10,6);
 
+    this.fulcrumIndex;
 	this.make_limb_list();
 
 	this.origCenterOfMass = this.get_center_of_mass(); // set origCenterOfMass to correct position
 
 }
 Man.prototype.make_limb_list = function(){
+	if (this.limbList.length > 0){				//Stickman already exists in space. Set neckBase accordingly
+    	this.neckBase = [this.limbList[0].pos1[0], this.limbList[1].pos1[1]];
+	}
+    //this.diff = [0,0];
+
 	this.limbList = [];
 	this.limbList.push(new Limb(this.limbAngles[0], this.limbLengths[0])); //torso
 
 	var backVector = angle_to_vector(this.limbAngles[0]);
-	this.limbList[0].pos1 = this.neckBase;
-	this.limbList[0].pos2 = [this.neckBase[0] + this.limbLengths[0] * backVector[0], this.neckBase[1] + this.limbLengths[1] * backVector[1]];
+
+	this.limbList[0].pos1 = [this.neckBase[0],this.neckBase[1] + this.diff[1]];
+	this.limbList[0].pos2 = [this.neckBase[0] + this.limbLengths[0] * backVector[0],
+        this.neckBase[1] + this.diff[1] + this.limbLengths[0] * backVector[1]];
+
+
 
 	this.limbList.push(new Limb(this.limbAngles[1], this.limbLengths[1])); //right upper arm
 	this.limbList.push(new Limb(this.limbAngles[2], this.limbLengths[2])); //left upper arm
@@ -175,6 +238,10 @@ Man.prototype.update_limb_angles = function(){
 		this.limbList[i].angle = this.limbAngles[i];
 	}
 	this.set_limb_ends(); //update endpoints to the limbs according to the new angles
+    if (this.ONGROUND) {
+    	this.is_on_ground(); // if below ground, put on ground
+        this.put_down_on_ground(); // if above ground, put on ground
+    }
 }
 
 
@@ -182,7 +249,8 @@ Man.prototype.draw = function(){
 	//draw head
  	var headPos = [this.diff[0] + this.limbList[0].get_pos1()[0] - .2*(this.limbList[0].get_pos2()[0] - this.limbList[0].get_pos1()[0]), 
  				this.diff[1] + this.limbList[0].get_pos1()[1] - .2*(this.limbList[0].get_pos2()[1] - this.limbList[0].get_pos1()[1])]
- 	context.beginPath();
+ 	headPos[0] = headPos[0]%width;
+    context.beginPath();
 	context.arc(headPos[0], headPos[1], 8, 2*Math.PI, false);
 	context.fillStyle = "#000000";
 	context.fill();
@@ -198,17 +266,14 @@ Man.prototype.draw = function(){
 		 	this.limbList[limb].draw(this.diff);
 		 }
 	}
- 	context.beginPath();
-	context.arc(this.origCenterOfMass[0] + this.diff[0], this.origCenterOfMass[1] + this.diff[1], 4, 2*Math.PI, false);
-	context.fillStyle = "#000000";
-	context.fill();
+
 }
 Man.prototype.set_limb_ends = function(){
 	var limb_ends = this.get_limb_ends();
-	for (var limb in this.limbList){
-		this.limbList[limb].pos1 = limb_ends[limb][0];
-		this.limbList[limb].pos2 = limb_ends[limb][1];
-	}
+	for (var limb in this.limbList) {
+        this.limbList[limb].pos1 = limb_ends[limb][0];
+        this.limbList[limb].pos2 = limb_ends[limb][1];
+    }
 }
 Man.prototype.calc_rot_angle = function(){
 	// return 0;
@@ -366,12 +431,13 @@ Man.prototype.get_moment = function(pos){
 	var moment = 0;
 	var distx, disty;
 	for (var limb in this.limbList){
-		distx = 0.5 * (this.limbList[limb].get_pos1()[0] + this.limbList[limb].get_pos2()[0]);
-		disty = 0.5 * (this.limbList[limb].get_pos1()[1] + this.limbList[limb].get_pos2()[1]);
-		moment += distx * distx + disty * disty * this.limbList[limb].get_length();
+		distx = 0.5 * (this.limbList[limb].get_pos1()[0] + this.limbList[limb].get_pos2()[0]) + this.diff[0] - pos[0];
+		disty = 0.5 * (this.limbList[limb].get_pos1()[1] + this.limbList[limb].get_pos2()[1]) + this.diff[1] - pos[1];
+		moment += (distx * distx + disty * disty) * this.limbList[limb].get_length();
 	}
 
-	moment +=  headWeight * Math.pow(get_dist(this.get_head_pos(), pos),2);
+    //var head_pos = this.get_head_pos();
+    //moment +=  headWeight * Math.pow(get_dist([head_pos[0] + this.diff[0], head_pos[1] + this.diff[1]], pos),2);
 	return moment;
 }
 Man.prototype.rotate_rigid_man = function(pos, incAngle){
@@ -412,9 +478,6 @@ Man.prototype.rotate_rigid_man = function(pos, incAngle){
 
 	}
 }
-Man.prototype.get_location = function(){
-	return [this.origCenterOfMass[0] + this.diff[0]]
-}
 Man.prototype.update_diff_by_accel = function(accel){
 	for (var i = 0; i < 2; i++){
 		this.diff[i] += this.vel[i] * dt + 0.5 * accel[i] * Math.pow(dt, 2);
@@ -422,44 +485,95 @@ Man.prototype.update_diff_by_accel = function(accel){
 	}
 }
 Man.prototype.update_diff_by_cm = function(){
-	var new_cm = this.get_center_of_mass()
-	this.diff[0] += new_cm[0] - this.origCenterOfMass[0];
-	this.diff[1] += new_cm[1] - this.origCenterOfMass[1];
+    //move diff to keep cm in same place
+	var new_cm = this.get_center_of_mass();
+	this.diff[0] -= (new_cm[0] - this.origCenterOfMass[0]);
+    this.diff[1] -= (new_cm[1] - this.origCenterOfMass[1]);
+
+    //if ONGROUND, put stickman back down on ground
+    if (this.ONGROUND) {
+    	this.is_on_ground(); // if below ground, put on ground
+        this.put_down_on_ground(); // if above ground, put on ground
+    }
+
 	this.origCenterOfMass = new_cm;
+
 }
 Man.prototype.is_on_ground = function(){
+    var below_ground_flag = false;
+
 	for (var limb in this.limbList){
-		if (this.limbList[limb].get_pos2()[1] + this.diff[1] > ground){
-			return true;
+		if (this.limbList[limb].get_pos2()[1] + this.diff[1] >= ground){
+            this.diff[1] = ground - this.limbList[limb].get_pos2()[1]; //set back on ground
+            this.fulcrumIndex = limb;
+			below_ground_flag = true;
 		}
 	}
 
-	if (this.get_head_pos()[1] > ground){
-		return true;
+	if (this.get_head_pos()[1] + this.diff[1] >= ground){
+        this.diff[1] = ground - this.get_head_pos()[1]; //set back on ground
+        this.fulcrumIndex = 10;
+		below_ground_flag = true;
 	}
-	return false;
+
+    if (below_ground_flag) {
+        this.netChange = 0;
+        this.rotationalMomentum = 0;
+    }
+    //if (this.ONGROUND && (!this.JUMP)){ //was on ground. Do not put him in the air
+    //    return true;
+    //}
+	return below_ground_flag;
 }
+Man.prototype.put_down_on_ground = function(){
+    var highest_y = 0;
+	var highest_index;
+
+	for (var limb in this.limbList){
+		if (highest_y < (this.limbList[limb].get_pos2()[1] + this.diff[1]) &&
+            (this.limbList[limb].get_pos2()[1] + this.diff[1]) <= ground){
+			highest_y = this.limbList[limb].get_pos2()[1] + this.diff[1];
+            highest_index = limb;
+		}
+	}
+
+
+	if (this.get_head_pos()[1] + this.diff[1] > highest_y){
+		//put head on ground
+        this.diff[1] = ground - this.get_head_pos()[1]; //set back on ground
+	}
+    else if (highest_index != null) {
+        this.diff[1] = ground - this.limbList[highest_index].get_pos2()[1]; //set back on ground
+    }
+}
+
 Man.prototype.conform_rigid_man = function(){
 
-	var newLimbAngles = [-30*rad,-45*rad,10*rad,10*rad,35*rad,3*rad,10*rad,-25*rad,-5*rad];
+	var newLimbAngles;
 
 	if (this.STATE == STANDING){
-		newLimbAngles = [-30*rad,-45*rad,10*rad,10*rad,35*rad,3*rad,10*rad,-25*rad,-5*rad];
+		newLimbAngles = [0*rad,-15*rad,20*rad,50*rad,25*rad,-10*rad,15*rad,-10*rad,-15*rad];
 	}
 	else if (this.STATE == ARCHED){
-		newLimbAngles = [10*rad,210*rad,230*rad,190*rad,220*rad,10*rad,50*rad,35*rad,45*rad];
+		newLimbAngles = [0*rad,210*rad,190*rad,50*rad,50*rad,-4*rad,32*rad,-35*rad,-45*rad];
 	}
 	else if (this.STATE == HOLLOW){
-		newLimbAngles = [0*rad,160*rad,110*rad,170*rad,150*rad,-20*rad,30*rad,-30*rad,-35*rad];
+		newLimbAngles = [0*rad,130*rad,150*rad,50*rad,30*rad,0*rad,25*rad,-10*rad,-15*rad];
 	}
 	else if (this.STATE == TUCKED){
-		newLimbAngles = [-30*rad,-40*rad,-25*rad,35*rad,45*rad,105*rad,120*rad,-20*rad,-30*rad];
+		newLimbAngles = [0*rad,-26*rad,-15*rad,125*rad,105*rad,165*rad,151*rad,-175*rad,-165*rad];
 	}
 	else if (this.STATE == LONG){
-		newLimbAngles = [0*rad,-150*rad,150*rad,-170*rad,170*rad,-20*rad,20*rad,-10*rad,10*rad];
+		newLimbAngles = [0*rad,-160*rad,160*rad,-10*rad,10*rad,-15*rad,15*rad,10*rad,-10*rad];
 	}
 	else if (this.STATE == RUNNING){
-		//complete..
+		if (this.runTime < this.stridePeriod/2){
+            newLimbAngles = [0*rad,-45*rad,50*rad,90*rad,75*rad,-50*rad,55*rad,-10*rad,-35*rad];
+        }
+        else{
+            newLimbAngles = [0*rad,50*rad,-45*rad,75*rad,90*rad,55*rad,-50*rad,-35*rad,-10*rad];
+        }
+        this.runTime = (this.runTime + 1) % this.stridePeriod;
 	}
 
 	//flip sign of angles if facing left
@@ -470,20 +584,47 @@ Man.prototype.conform_rigid_man = function(){
 	}
 
 	//update limb angles and calculate netChange
+    this.netChange = 0;
 	for (var i in this.limbAngles){
-		this.limbAngles[i] += 0.15 * (newLimbAngles[i] - this.limbAngles[i]);
-		this.netChange += 3000 * (newLimbAngles[i] - this.limbAngles[i]);
+		this.limbAngles[i] += 0.1 * (newLimbAngles[i] - this.limbAngles[i]);
+		this.netChange -= 1 * Math.pow(10,3) * (newLimbAngles[i] - this.limbAngles[i]);
 	}
+
 }
 Man.prototype.set_state = function(char){
 	this.STATE = char;
 }
 Man.prototype.set_right = function(){
 	this.RIGHT = true;
+    if (this.ONGROUND) {
+        this.make_limb_list();
+        if (this.STATE == RUNNING) {
+            this.diff[0] += 5;
+        }
+    }
+    //var baseAngle = this.calc_rot_angle();
+    //this.rotate_rigid_man(this.get_location(), -baseAngle);
 }
 Man.prototype.set_left = function(){
 	this.RIGHT = false;
+    if (this.ONGROUND) {
+        this.make_limb_list();
+        if (this.STATE == RUNNING) {
+            this.diff[0] -= 5;
+        }
+    }
+}
+Man.prototype.set_jump = function(){
+    this.JUMP = true;
 }
 Man.prototype.get_location = function(){
-	return [this.origCenterOfMass[0] + this.diff[0], this.origCenterOfMass[1] + this.diff[1]];
+    var cm = this.get_center_of_mass();
+	return [cm[0] + this.diff[0], cm[1] + this.diff[1]];
+}
+Man.prototype.jump = function(){
+    this.vel[1] = -12;
+    man.update_diff_by_accel(g);
+    this.rotationalMomentum = this.netChange;
+    this.JUMP = false;
+    this.ONGROUND = false;
 }
